2f5d530d144e9cb81e6eb7c15255d72ec30c793f
Sathiyanarayanan <sathiyastar2000@gmail.com>
2021-05-28 00:16:36 +0530

    Staff_access created

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import QuizView
+from .views import QuizView,QuizPlayers
 
 urlpatterns = [
     path('',QuizView.admin_console,name='staff'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('quiz-list/',QuizView.quizlist,name='quiz-list'),
     path('staff-list/',QuizView.staflist,name='staff-list'),
     path('delete-quiz/<str:quiz_id>/',QuizView.delete_quiz,name='delete-quiz'),
+    path('restricted_access',QuizPlayers.restricted_access,name='restricted_access'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,30 +1,33 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.admin.views.decorators import staff_member_required
 from django.core import serializers
 from django.http import HttpResponse
 from .models import Questions,QuizModel
 
 # Create your views here.
-@login_required
 class QuizView:
+    @login_required
     def dashboard(request):
         return render(request,'dashboard.html')
-    
+
+    @user_passes_test(lambda u: (u.is_staff and u.is_active),login_url='restricted_access')
     def admin_console(request):
         if request.user.is_staff:
             return render(request,'admin/staff_console.html')
         else:
             return HttpResponse("<script>alert('Unauthorized Usage'); window.location.href = '/dashboard';</script>")
     
+    @user_passes_test(lambda u: (u.is_superuser),login_url='restricted_access')
     def create_staff(request):
         if request.user.is_superuser:
             if request.method == 'POST':
-
                 return HttpResponse('post staff')
             return render(request,'admin/create_staff.html')
         else:
             return HttpResponse("<script>alert('Unauthorized Usage'); window.location.href = '/dashboard';</script>")
-    
+   
+    @user_passes_test(lambda u: (u.is_active and u.is_staff),login_url='restricted_access')
     def create_quiz(request):
         if request.user.is_staff:
             if request.method == 'POST':
@@ -47,13 +50,27 @@ class QuizView:
                 return render(request,'admin/create_quiz.html',{"range":range(10)})
         else:
             return HttpResponse("<script>alert('Unauthorized Usage'); window.location.href = '/dashboard';</script>")
-    
+    @user_passes_test(lambda u: (u.is_active and u.is_staff),login_url='restricted_access')
     def quizlist(request):
         return render(request,'admin/quiz_list.html',{"quizzes_list":QuizModel.objects.all()})
-    
+
+    @user_passes_test(lambda u: (u.is_staff and u.is_active) ,login_url='restricted_access')
     def staflist(request):
         return render(request,'admin/staff_list.html')
-    
+
+    @user_passes_test(lambda u: (u.is_active and u.is_staff),login_url='restricted_access')
     def delete_quiz(request,quiz_id):
         QuizModel.objects.get(id=quiz_id).delete()
         return redirect('/staff/quiz-list')
+
+class QuizPlayers:
+    def restricted_access(request):
+        if request.user.is_active and request.user.is_authenticated:
+            if request.user.is_staff:
+                return HttpResponse("<script>alert('Unauthorized Usage'); window.location.href = '/staff/';</script>")
+            else:
+                return HttpResponse("<script>alert('Unauthorized Usage'); window.location.href = '/dashboard/';</script>")
+        else:
+            return HttpResponse("<script>alert('Unauthorized Usage'); window.location.href = '/login/';</script>")
+
+        
