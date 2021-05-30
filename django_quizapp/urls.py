4d610555d17f7d0ff6cf7560d3ed6959af85ab36
Sathiyanarayanan <sathiyastar2000@gmail.com>
2021-05-30 20:31:49 +0530

    Deployment

--- django_quizapp/urls.py
+++ django_quizapp/urls.py
@@ -18,10 +18,12 @@ from django.urls import path,include
 from django.contrib.auth.views import LogoutView
 from authentication.views import Authentication
 from quiz.views import QuizView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('authentication.urls')),
     path('admin/', admin.site.urls),
     path('staff/',include('quiz.urls')),
     path('user/',include('quiz.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
